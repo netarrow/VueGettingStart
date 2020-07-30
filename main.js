5ff96bd58183d066b307703921a7524108b4ebaf
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2020-07-30 15:10:59 +0200

    testing conditional binding, added styles

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ var app = new Vue({
         title: 'Vue app title',
         subtitle: 'subtitle',
         foods: [
-            {name: 'apple', description: 'test', src: 'https://i5.walmartimages.ca/images/Large/094/514/6000200094514.jpg'},
-            {name: 'eggs', description: 'test2', src: 'https://i0.wp.com/ajp.com.au/wp-content/uploads/2019/11/Eggs.png'},
-            {name: 'banana', description: 'test3', src: 'https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SL1500_.jpg'}]
+            {name: 'apple', description: 'test', 
+            src: 'https://i5.walmartimages.ca/images/Large/094/514/6000200094514.jpg', percent_carbs: 79, is_fav: true},
+            {name: 'eggs', description: 'test2', 
+            src: 'https://i0.wp.com/ajp.com.au/wp-content/uploads/2019/11/Eggs.png',  percent_carbs: 2, is_fav: true},
+            {name: 'banana', description: 'test3', 
+            src: 'https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SL1500_.jpg',  percent_carbs: 93, is_fav: false}]
     }
 });
